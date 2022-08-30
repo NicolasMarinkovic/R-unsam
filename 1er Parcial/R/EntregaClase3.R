e5524c040307b1c3e0a413154f314652b9aff478
Nicolas Marinkovic <nicolasnahuel01@gmail.com>
2022-08-30 02:08:43 -0300

    Entrega 3 complete

--- 1er Parcial/R/EntregaClase3.R
+++ 1er Parcial/R/EntregaClase3.R
@@ -3,15 +3,23 @@
 #en la salida y tiempo ganado en el aire.
 library(tidyverse)
 library(nycflights13)
+library(ggplot2)
+library(ggridges)
 
 df_1 <- group_by(flights, month)
 df_2 <- summarise(df_1, nvuelos = n(),
-                  nganado=sum(arr_delay<dep_delay, na.rm=TRUE),
-                  total =(nganado/nvuelos)*100)
+                  nganado=sum((arr_delay<0)<(dep_delay>0), na.rm=TRUE),
+                  total =(nganado/nvuelos)*100,
+                  Media=mean(arr_delay, na.rm=TRUE), 
+                  Mediana=median(arr_delay, na.rm=TRUE),
+                  DesvEstandar=sd(arr_delay, na.rm=TRUE),
+                  DistInter=IQR(arr_delay, na.rm=TRUE))
 
-ggplot(data=df_2) + geom_bar(aes(x=month, y=total, color= month), 
-                             fill= 'Red', width=0.6,
-                            stat = 'identity') +
-                     xlim(0,12) + 
-  labs(x='Meses', y='Retraso en la partida [min]', 
-       title='Retraso de las aerolinea US')
+ggplot(data=df_2) + geom_bar(aes(x=month, y=Media, fill= month)
+                        ,stat = 'identity') +
+                        xlim(0,13) +
+  labs(x='Meses', y='Media de tiempo de vuelos tardíos en la llegada', 
+       title='Promedio de tardanza en Aeropuertos US',
+       subtitle='Vuelos de septiembre 2013')
+
+            
